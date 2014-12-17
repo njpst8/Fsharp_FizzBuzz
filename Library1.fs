e2bfe42608aeaa7b3ae177aaa155fea03cd68c91
Nate <nathanj.peterson@gmail.com>
2014-12-17 13:33:16 -0500

    use tuples to match

--- Library1.fs
+++ Library1.fs
@@ -5,3 +5,33 @@ open NUnit.Framework
 [<Test>] 
 let testFrameworkSetup() = 
     Assert.True(true)
+
+
+let transform number = 
+    let fizzBuzzTuple = (number % 3, number % 5)
+    match fizzBuzzTuple with
+    | (0, 0) -> "FizzBuzz"
+    | (0, _) -> "Fizz"
+    | (_, 0) -> "Buzz"
+    | _ -> string number
+
+
+[<Test>] 
+let threeReturnsFizz() = 
+    Assert.AreEqual("Fizz", transform 3)
+
+[<Test>] 
+let sixReturnsFizz() = 
+    Assert.AreEqual("Fizz", transform 6)
+    
+[<Test>] 
+let fiveReturnsBuzz() = 
+    Assert.AreEqual("Buzz", transform 5)
+        
+[<Test>] 
+let fifteenReturnsFizzBuzz() = 
+    Assert.AreEqual("FizzBuzz", transform 15)
+    
+[<Test>] 
+let thirteenReturns13() = 
+    Assert.AreEqual("13", transform 13)
